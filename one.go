50e1bc290f1273d038ac04565c231d0fdee2d320
TusharYousuf <yousufhossen31@gmail.com>
2025-09-13 00:22:32 +0300

    my if else project is done

--- one.go
+++ one.go
@@ -1,7 +1,23 @@
-package main 
+package main
 
 import "fmt"
 
-func main () {
-fmt. Println ("Hello, Go I am yousuf Hossain")
+func main() {
+    age := 20
+
+    if age >= 18 {
+        fmt.Println("You are eligible to vote.")
+    } else {
+        fmt.Println("You are not eligible to vote.")
+    }
+
+    temperature := 25
+
+    if temperature < 0 {
+        fmt.Println("It's freezing!")
+    } else if temperature < 20 {
+        fmt.Println("It's a bit chilly.")
+    } else {
+        fmt.Println("It's a pleasant temperature.")
+    }
 }
