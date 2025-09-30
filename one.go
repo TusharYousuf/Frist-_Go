304e4a6997f7537d25a0b594bb98500b19abd2b8
TusharYousuf <yousufhossen31@gmail.com>
2025-09-30 05:46:46 +0300

    string is   finished

--- one.go
+++ one.go
@@ -1,21 +1,32 @@
-// simple Go program to illustrate
-//how to create a rune
- package main
+package main 
 
- import (
+import (
 	"fmt"
-	"reflect"
- )
+	"strings"
 
+)
  func main() {
-
-	// creating a rune
-	rune1 := 'B'
-	rune2 := 'g'
-	rune3 := '\a'
-	//Displaying rune and its type
-	fmt.Printf("Rune 1: %c Unicode: %U; Type: %s", rune1, rune1, reflect. TypeOf(rune1))
-	fmt.Printf("\nRune 2: %c; Unicode: %U; Type: %s", rune2, rune2, reflect. TypeOf(rune2))
-	fmt.Printf("\nRune 3: Unicode: %U; Type: %s", rune3, reflect. TypeOf(rune3))
+	// string example
+	var myString = "Hello, world"
+	//concatenation 
+	str1 := "Hello, "
+	str2 := "world!"
+	result := str1 + str2
+	fmt.Println(result) // Output: Hello, World!
+	//slicing
+	myString = "Hello, world!"
+	fmt.Println(myString[0:5]) // Output: Hello
+	// indexing
+	fmt.Println(myString[7]) // Output: w
+	// length of a string
+	fmt.Println(len(myString)) // Output: 13
+	// converting a string to uppercase
+	fmt.Println(strings. ToUpper(myString)) // Output: HELLO, WORLD!
+	// converting a string to lowercase 
+	fmt.Println(strings. ToLower(myString)) // Output: hello, world!
+	// splitting a string
+	str := "one, two, three, four, five"
+	splitStr := strings.Split(str, ",")
+	fmt.Println(splitStr) // Output: [one two three four five]
 
  }
