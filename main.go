3ed119811cab848511f893a770f8ff7b2b8c5016
TusharYousuf <yousufhossen31@gmail.com>
2025-10-03 03:21:20 +0300

    i try to go lang

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main 
-import "fmt"
+
+import (
+	"fmt"
+)
 
 func main() {
-	c1 := complex(2,3)
-	c2 := 4 + 5i 			   // complex initializer syntax a + ib
-	c3 := c1 + c2 	  		  // addition just like other variable 
-	fmt.Println("Add: ", c3) // prints "Add: (6+8i)"
-	re := real(c3)    		// get real part
-	im := imag(c3) 		   // get imaginary part
-	fmt.Println(re, im)   // prints 6 8
-
+	numberCh := make(chan int, 10)
+	for i := 1; i <= 10; i++ {
+		numberCh <- i
+	}
+	for j := 1; j <= 10; j++ {
+		fmt.Println(<-numberCh)
+	}
 }
