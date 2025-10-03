6372ed6c6b2ce0b05d7a076f0544212f2f4be0c9
TusharYousuf <yousufhossen31@gmail.com>
2025-10-03 05:12:59 +0300

    now i'm try to operators

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main 
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	numberCh := make(chan int, 10)
-	for i := 1; i <= 10; i++ {
-		numberCh <- i
-	}
-	for j := 1; j <= 10; j++ {
-		fmt.Println(<-numberCh)
-	}
+	p := 34
+	q := 20
+	// Addition
+	result1 := p + q
+	fmt.Printf("Result of p + q = %d", result1)
+	//Subtraction
+	result2 := p - q 
+	fmt.Printf("\nResult of p - q = %d", result2)
+	//Multiplication
+	result3 := p * q
+	fmt.Printf("\nResult of p * q = %d", result3)
+	// Division
+	result4 := p / q 
+	fmt.Printf("\nResult of p / q = %d", result4)
+	// Modulus
+	result5 := p % q
+	fmt.Printf("\nResult of p %% q = %d", result5)
+
 }
