66bda9aab7a902e0e6f95b63663e93839f5ddc47
TusharYousuf <yousufhossen31@gmail.com>
2025-10-05 03:24:33 +0300

    Comparison Operators is learning

--- one.go
+++ one.go
@@ -1,24 +1,27 @@
-package main 
+package main
 
 import "fmt"
 
 func main() {
 	p := 34
 	q := 20
-	// Addition
-	result1 := p + q
-	fmt.Printf("Result of p + q = %d", result1)
-	//Subtraction
-	result2 := p - q 
-	fmt.Printf("\nResult of p - q = %d", result2)
-	//Multiplication
-	result3 := p * q
-	fmt.Printf("\nResult of p * q = %d", result3)
-	// Division
-	result4 := p / q 
-	fmt.Printf("\nResult of p / q = %d", result4)
-	// Modulus
-	result5 := p % q
-	fmt.Printf("\nResult of p %% q = %d", result5)
+	// '==' (Equal To)
+	result1 := p == q
+	fmt.Println(result1)
+	// '!='(Not equal To)
+	result2 := p != q
+	fmt.Println(result2)
+	// '>'(Greater Than)
+	result3 := p > q
+	fmt.Println(result3)
+	// '<'(Less Than)
+	result4 := p < q
+	fmt.Println(result4)
+	// '>='(Greater Than Equal To)
+	result5 := p >= q
+	fmt.Println(result5)
+	// '<='(Less Than Equal To)
+	result6 := p <= q
+	fmt.Println(result6)
 
 }
