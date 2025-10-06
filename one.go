f24c8fc0da5399fed27c455dafeaa79413eaab14
TusharYousuf <yousufhossen31@gmail.com>
2025-10-06 04:00:34 +0300

    switch program is learning

--- one.go
+++ one.go
@@ -3,15 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var p int = 23
-	var q int = 60
-	if p != q && p <= q {
-		fmt.Println("True")
-	}
-	if p != q || p <= q {
-		fmt.Println("True")
-	}
-	if !(p == q) {
-		fmt.Println("True")
+	day := "Friday"
+	switch day {
+	case "Monday":
+		fmt.Println("It's Monday, the start of the wee!")
+	case "Tuesday":
+		fmt.Println("It's Tuesday, still a long way to go!")
+	case "Wednesday":
+		fmt.Println("It's Wednesday, middle of the week!")
+	case "Thursday":
+		fmt.Println("It's Thursday, almost there!")
+	case "Friday":
+		fmt.Println("It's Friday, the end of week!")
+	default:
+		fmt.Println("It's the weekend!")
+
 	}
 }
