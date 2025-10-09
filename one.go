f84c19cc1b9fbd32f4079cf0f3e92ef5e67ebc1c
TusharYousuf <yousufhossen31@gmail.com>
2025-10-09 05:04:18 +0300

    i try to ranges

--- one.go
+++ one.go
@@ -3,20 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	day := "Friday"
-	switch day {
-	case "Monday":
-		fmt.Println("It's Monday, the start of the wee!")
-	case "Tuesday":
-		fmt.Println("It's Tuesday, still a long way to go!")
-	case "Wednesday":
-		fmt.Println("It's Wednesday, middle of the week!")
-	case "Thursday":
-		fmt.Println("It's Thursday, almost there!")
-	case "Friday":
-		fmt.Println("It's Friday, the end of week!")
-	default:
-		fmt.Println("It's the weekend!")
-
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
 	}
+	fmt.Println("sum:", sum)
 }
