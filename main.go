4d36bf3bae135d33249d017d6a052bbb3d8e1b21
TusharYousuf <yousufhossen31@gmail.com>
2025-12-07 05:53:27 +0300

    allah vorosha

--- main.go
+++ main.go
@@ -2,27 +2,48 @@ package main
 
 import "fmt"
 
-// implement the function sumAll(arg... int) here
-func sumAll(args...int) int{
-	sum := 0
-	for _, v := range args {
-		sum += v
-	}
-	return sum
-}
-func main() {
-    arr1 := []int{1, 2, 3}
-    arr2 := []int{1, 2, 3, 4}
-    arr3 := []int{1, 2, 3, 4, 5}
-    result1 := sumAll(arr1...)
-    result2 := sumAll(arr2...)
-    result3 := sumAll(arr3...)
-    fmt.Println("sum of first array ", result1)
-    fmt.Println("sum of second array ", result2)
-    fmt.Println("sum of third array ", result3)
+func printWelcomeMessage() {
+    fmt.Println("Welcome to the application")
 }
 
-// Output :
-// sum of first array 6
-// sum of first array 10
-// sum of first array 15
+func getUserName() string {
+       var name string 
+    fmt.Println("Enter your name - ")
+    fmt.Scanln(&name)
+    return name
+}
+
+func getTwoNumbers() (int, int) {
+     var num1 int 
+    var num2 int
+    fmt.Println("Enter first number - ")
+    fmt.Scanln(&num1) // & => ampersand
+    fmt.Println("Enter second number - ")
+    fmt.Scanln(&num2)
+    return num1, num2
+
+}
+
+func add(num1 int, num2 int) int {
+    sum := num1 + num2
+    return sum
+}
+
+func display(name  string, sum int) {
+     fmt.Println("Hello, ", name)
+    fmt.Println("Summation = ", sum)
+}
+
+func printGoodbyeMessage() {
+       fmt.Println("Thank you for using the application")
+     fmt.Println("Goodbye")
+}
+func main() {
+    printWelcomeMessage()
+    name := getUserName()
+    num1, num2 := getTwoNumbers()
+    sum := add(num1, num2)
+    display(name, sum)
+    printGoodbyeMessage()
+
+}
